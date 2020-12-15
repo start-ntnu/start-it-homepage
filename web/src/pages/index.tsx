0eba71fb90ebc50a1889588efa6fa04defb9512f
Casper Feng <casp.feng@gmail.com>
2020-12-15 19:55:54 +0100

    update landing page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { graphql } from "gatsby";
 import React from "react";
+import Typing from 'react-typing-animation';
 import styled from "styled-components";
 import Layout from "../components/Layout";
+
 export const query = graphql`
   query {
     StartNTNU: file(relativePath: { eq: "Startlogo.jpg" }) {
@@ -19,18 +21,21 @@ export default function App({ data }) {
     <Layout>
       <Container>
         <Title><span style={{color: '#BD6631'}}>START</span>IT 2021</Title>
+        <Typing>
+        <SubTitle>En innovativ konferanse.</SubTitle>
+        </Typing>
         <Article>
           {" "}
           På NTNU utdannes det for øyeblikket mange tusen studenter som snart
-          skal ut i jobb og må finne ut hva de vil jobbe med. <br></br>
-          <br></br>
+          skal ut i jobb og må finne ut hva de vil jobbe med.
           StartIT har som formål å motivere og inspirere studentene ved NTNU til
           å ville jobbe med å løse problemer ved hjelp av innovasjon og
-          teknologien som de lærer her på NTNU. <br></br>
-          <br></br>Vi ønsker å skifte fokuset fra “hvor kan man tjene mest
+          teknologien som de lærer her på NTNU.
+          <br /><br />
+        Vi ønsker å skifte fokuset fra “hvor kan man tjene mest
           penger” til “hvordan kan man være med å løse de mest spennende
           problemene vi står ovenfor i dag”.
-          <br></br> <br></br>Dette forsøker vi å gjøre ved å vise mulighetene
+         Dette forsøker vi å gjøre ved å vise mulighetene
           som finnes til å jobbe med innovasjon innenfor teknologi. Det kan være
           alt fra å vise frem de mest innovative inhouse bedriftene, de mest
           kreative konsulentselskapene, de kuleste startupsene, eller hvordan
@@ -41,14 +46,20 @@ export default function App({ data }) {
   );
 }
 
+const SubTitle = styled.h2`
+  margin: 0 auto;
+  justify-content: center;
+  text-align: center;
+`;
+
 const Article = styled.p`
   font-size: 20px;
+  line-height: 2;
   display: flex;
   flex-direction: column;
   justify-content: center;
   text-align: center;
-  margin-left: auto;
-  margin-right: auto;
+  margin: 24px auto 10px auto;
   @media (max-width: 1268px) {
     font-size: 16px;
   }
@@ -72,5 +83,5 @@ const Container = styled.div`
   display: flex;
   align-content: center;
   flex-direction: column;
-  max-width: 1200px;
+  max-width: 900px;
 `
